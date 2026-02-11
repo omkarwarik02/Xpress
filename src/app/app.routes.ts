d2b50caf52557aeac4f91f1873806d9778946dc5
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-02-11 12:16:06 +0530

    Created login function and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login-component/login-component';
+import { RegisterComponent } from './components/register-component/register-component';
+import { HomeComponent } from './components/home-component/home-component';
 
 export const routes: Routes = [
-    {path: "login", component:LoginComponent}
+    {path: "login", component:LoginComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"home",component:HomeComponent}
 ];
--- src/app/components/login-component/login-component.ts
+++ src/app/components/login-component/login-component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormGroup,FormControl,ReactiveFormsModule } from '@angular/forms';
-
+import { FormGroup,FormControl,ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 @Component({
   selector: 'app-login-component',
   imports: [ButtonModule,DividerModule,InputTextModule, ReactiveFormsModule],
@@ -11,13 +12,33 @@ import { FormGroup,FormControl,ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
+
+  private router = inject(Router);
+  private authService = inject(Auth);
+
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required])
   });
 
+register(){
+  this.router.navigate(['/register']);
+}
+onSubmit(){
+  if(this.loginForm.invalid){
+    this.loginForm.markAllAsTouched();
+  }
+  const {email, password}=this.loginForm.value;
 
-
+  this.authService.login(email!,password!).subscribe({
+    next:()=>{
+      this.router.navigate(['/home']);
+    },
+    error:(err)=>{
+      alert(err.error?.message || 'Login failed');
+    }
+  })
+}
 
 
 }
